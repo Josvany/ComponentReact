eab1190ee845c471dc8338bdb2f3f8af1ee3e81e
Josvany García <jgarcia@cgclatam.com>
2022-02-09 16:06:03 -0600

    update component

--- src/components/speakers.js
+++ src/components/speakers.js
@@ -2,13 +2,15 @@ import { data } from './../../SpeakerData';
 import Header from './header';
 import SpeakerList from './SpeakerList';
 import SpeakersToolbar from './SpeakersToolbar';
+import { useState } from 'react';
 
 function Speakers()
 {
+    const [theme, setTheme] = useState("light");
     return (
-        <div className="container-fluid">
-            <Header />
-            <SpeakersToolbar /> 
+        <div className={theme === "light" ? "container-fluid light": "contaniner-fluid dark"}>
+            <Header  theme={theme}/>
+            <SpeakersToolbar theme={theme} setTheme={setTheme} /> 
             <SpeakerList data={data} />
         </div>
         
